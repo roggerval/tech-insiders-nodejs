0e7ac87b96f1ddb4157b254866e25171a178b73e
rogger andré valverde flores <rogger.valverde@uni.pe>
2020-06-22 20:34:31 -0500

    feat(server): initializing apollo server

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('TECH INSIDERS NODEJS');
+import { ApolloServer, gql } from 'apollo-server';
+
+const typeDefs = gql`
+  # Este es un comentario
+  type User {
+    names: String
+    lastNames: String
+  }
+
+  """
+  Este es otro comentario
+  """
+  type Query {
+    users: [User]
+  }
+`;
+
+const server = new ApolloServer({ typeDefs });
+
+server.listen({ port: process.env.PORT }).then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
